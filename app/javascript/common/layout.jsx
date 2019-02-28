a83edf4bc01fcf08d88f77d1726f018e13b1fce2
jfcumsille <jfcumsille@uc.cl>
2019-02-27 22:56:20 -0300

    Added navbar

--- app/javascript/common/layout.jsx
+++ app/javascript/common/layout.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
+import { Navbar } from 'react-bootstrap' 
 
 class ApplicationLayout extends React.Component {
     render() {
         return (
-            <div className='header'>
-                <ul>
-                    <li>Rock'n Dress</li>
-                    <li>Home</li>
-                    <li>Cómo Funciona</li>
-                    <li>Arrendar</li>
-                    <li>Nosotras</li>
-                </ul>
-            </div>
+            <ul className="navbar">
+                <li className="navbar-logo"><a href="#home">Rock'n Dress</a></li>
+                <li className="navbar-item"><a href="#news">Home</a></li>
+                <li className="navbar-item"><a href="#news">Agenda tu Hora</a></li>
+                <li className="navbar-item"><a href="#contact">Cómo Funciona</a></li>
+                <li className="navbar-item"><a href="#about">Arrendar</a></li>
+                <li className="navbar-item"><a href="#about">More</a></li>
+            </ul>
         )
     }
 }
