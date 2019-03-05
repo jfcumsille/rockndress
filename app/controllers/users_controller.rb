e67ce70855e0f1783f406400318ef48c72a7f23e
jfcumsille <jfcumsille@uc.cl>
2019-03-04 21:36:41 -0300

    Header ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to root_path
+            log_in @user
+            flash[:success] = "Welcome to the Sample App!"
+            redirect_to @user
         else
             puts @user.errors.full_messages
         end
